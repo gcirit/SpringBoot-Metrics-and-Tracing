d85834e3ad24ff2b0c93681091b7659cb1480632
gcirit <gcirit@gmail.com>
2021-12-23 14:12:14 +0300

    Initial commit

---
+++ basic/client/src/test/java/com/gcirit/client/ClientApplicationTests.java
@@ -0,0 +1,15 @@
+package com.gcirit.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+
+@TestPropertySource(properties = "management.metrics.export.wavefront.enabled=false")
+@SpringBootTest
+class ClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
